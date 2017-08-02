120d9f41977819c5c3b008e710e94220720a5b67
Aparna <aparnajay.jay@gmail.com>
2017-08-02 16:14:20 -0700

    Create helper variables

---
+++ js/helper.js
@@ -0,0 +1,7 @@
+var HTMLrowStart = '<div class="row"></div>';
+
+var HTMLitemStart = '<div class="col-md-4"></div>';
+var HTMLimageUrl = '<img class="img-responsive image-margin" src="%data%"'
+var HTMLimageAlt = ' alt="%data%">'
+var HTMLlocationName = '<h3 class="main-color">New Zealand</h3>'
+var HTMLlocationDate = '<p class="sub-color italics">April 2017</p>'
