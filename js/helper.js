8874919fc4517ffea7a6fb6b70d720f17471a7b9
Aparna <aparnajay.jay@gmail.com>
2017-08-02 16:59:24 -0700

    Create display function

--- js/helper.js
+++ js/helper.js
@@ -1,7 +1,9 @@
+var test = '<p>look at me I work</p>';
+
 var HTMLrowStart = '<div class="row"></div>';
 
 var HTMLitemStart = '<div class="col-md-4"></div>';
 var HTMLimageUrl = '<img class="img-responsive image-margin" src="%data%"'
 var HTMLimageAlt = ' alt="%data%">'
-var HTMLlocationName = '<h3 class="main-color">New Zealand</h3>'
-var HTMLlocationDate = '<p class="sub-color italics">April 2017</p>'
+var HTMLlocationName = '<h3 class="main-color">%data%</h3>'
+var HTMLlocationDate = '<p class="sub-color italics">%data%</p>'
--- js/portfolioBuilder.js
+++ js/portfolioBuilder.js
@@ -22,6 +22,30 @@ var locations = {
 		"imageAlt": "Nepal",
 		"name": "Nepal",
 		"date": "October 2016"
-	},
-	]
-}
+	}],
+	"display": function () {
+		console.log ("this function is running");
+		var item = 0;
+		for (i = 0; i < (locations.locations.length/3); i++) {
+			$("#location").append(HTMLrowStart);
+
+			for (count=0; count < 3; count++) {
+				$(".row").append(HTMLitemStart);
+
+				var formattedImageUrl = HTMLimageUrl.replace("%data%", locations.locations[item].imageUrl);
+				var formattedImageAlt = HTMLimageAlt.replace("%data%", locations.locations[item].imageAlt);
+				var formattedImageStuff = formattedImageUrl + formattedImageAlt;
+				$(".col-md-4:last").append(formattedImageStuff);
+
+				var formattedLocationName = HTMLlocationName.replace("%data%", locations.locations[item].name);
+				$(".col-md-4:last").append(formattedLocationName);
+
+				var formattedDate = HTMLlocationDate.replace("%data%", locations.locations[item].date);
+				$(".col-md-4:last").append(formattedDate);
+				item++;
+			};
+		};
+	}
+};
+
+locations.display();
