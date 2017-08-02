aec4c288e81cafbb6696d0dddcd0ea2be464db5c
Aparna <aparnajay.jay@gmail.com>
2017-08-02 16:03:54 -0700

    Create JavaScript files

---
+++ js/portfolioBuilder.js
@@ -0,0 +1,27 @@
+var locations = {
+	"locations": [{
+		"imageUrl": "pictures/newzealand.jpg",
+		"imageAlt": "New Zealand Hobbiton",
+		"name": "New Zealand",
+		"date": "April 2017"
+	},
+	{
+		"imageUrl": "pictures/washingtondc.jpg",
+		"imageAlt": "Washington D.C.",
+		"name": "Washington D.C.",
+		"date": "March 2017"
+	},
+	{
+		"imageUrl": "pictures/hongkong.jpg",
+		"imageAlt": "Hong Kong",
+		"name": "Hong Kong",
+		"date": "October 2016"
+	},
+	{
+		"imageUrl": "pictures/nepal.jpg",
+		"imageAlt": "Nepal",
+		"name": "Nepal",
+		"date": "October 2016"
+	},
+	]
+}
