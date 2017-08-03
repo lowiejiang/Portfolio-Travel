e2b08fe5abf26cf7528c2f0343be03c8cc2ea18b
Aparna <aparnajay.jay@gmail.com>
2017-08-03 14:22:07 -0700

    Update map

--- js/portfolioBuilder.js
+++ js/portfolioBuilder.js
@@ -3,127 +3,148 @@ var travels = {
 		"imageUrl": "pictures/newzealand.jpg",
 		"imageAlt": "New Zealand Hobbiton",
 		"name": "New Zealand",
-		"date": "April 2017"
+		"date": "April 2017",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/washingtondc.jpg",
 		"imageAlt": "Washington D.C.",
 		"name": "Washington D.C.",
-		"date": "March 2017"
+		"date": "March 2017",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/hongkong.jpg",
 		"imageAlt": "Hong Kong",
 		"name": "Hong Kong",
-		"date": "October 2016"
+		"date": "October 2016",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/nepal.jpg",
 		"imageAlt": "Nepal",
 		"name": "Nepal",
-		"date": "October 2016"
+		"date": "October 2016",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/london.jpg",
 		"imageAlt": "London",
 		"name": "London",
-		"date": "June 2016"
+		"date": "June 2016",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/greece.jpg",
 		"imageAlt": "Greece",
 		"name": "Greece",
-		"date": "October 2015"
+		"date": "October 2015",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/madagascar.jpg",
 		"imageAlt": "Madagascar",
 		"name": "Madagascar",
-		"date": "December 2013"
+		"date": "December 2013",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/botswana.jpg",
 		"imageAlt": "Botswana",
 		"name": "Botswana",
-		"date": "July 2013"
+		"date": "July 2013",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/swaziland.jpg",
 		"imageAlt": "Swaziland",
 		"name": "Swaziland",
-		"date": "March 2013"
+		"date": "March 2013",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/southafrica.jpg",
 		"imageAlt": "South Africa",
 		"name": "South Africa",
-		"date": "December 2012"
+		"date": "December 2012",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/mozambique.jpg",
 		"imageAlt": "Mozambique",
 		"name": "Mozambique",
-		"date": "July 2012"
+		"date": "July 2012",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/lesotho.jpg",
 		"imageAlt": "Lesotho",
 		"name": "Lesotho",
-		"date": "October 2011"
+		"date": "October 2011",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/atlanta.jpg",
 		"imageAlt": "Atlanta",
 		"name": "Atlanta",
-		"date": "June 2011"
+		"date": "June 2011",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/vegas.jpg",
 		"imageAlt": "Las Vegas",
 		"name": "Las Vegas",
-		"date": "May 2011"
+		"date": "May 2011",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/india.jpg",
 		"imageAlt": "India",
 		"name": "Chennai",
-		"date": "December 2009"
+		"date": "December 2009",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/rome.jpg",
 		"imageAlt": "Rome",
 		"name": "Rome",
-		"date": "June 2008"
+		"date": "June 2008",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/singapore.jpg",
 		"imageAlt": "Singapore",
 		"name": "Singapore",
-		"date": "June 2007"
+		"date": "June 2007",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/newyork.jpg",
 		"imageAlt": "New York",
 		"name": "New York City",
-		"date": "March 2007"
+		"date": "March 2007",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/sf.jpg",
 		"imageAlt": "San Francisco",
 		"name": "San Francisco",
-		"date": "December 2006"
+		"date": "December 2006",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/sandiego.jpg",
 		"imageAlt": "San Diego",
 		"name": "San Diego",
-		"date": "June 2006"
+		"date": "June 2006",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	},
 	{
 		"imageUrl": "pictures/spain.jpg",
 		"imageAlt": "Spain",
 		"name": "Spain",
-		"date": "April 2005"
+		"date": "April 2005",
+		"location": {"lat": -42.017564, "lng": 173.397440}
 	}
 	],
 	"display": function () {
